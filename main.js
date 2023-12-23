88057207c97c02bf2412841c6ebb576db14a461f
zhura0well <slavonzhuravel@gmail.com>
2023-12-23 12:47:26 +0200

    [PA2] - implemented

--- main.js
+++ main.js
@@ -9,16 +9,38 @@ function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
 
+// constant variables for equations (can be chnged from html)
+let A = 0.3;
+let B = 0.3;
+let C = 0.15;
+
+function addListeners() {
+    let constants = ['a', 'b', 'c'];
+    window.addEventListener('DOMContentLoaded', () => {
+        constants.forEach((item) => {
+            document.getElementById(item).addEventListener("change", () => {
+                updateConstants();
+
+                surface.BufferData(CreateSurfaceData().vertexList);
+                draw();
+            });
+        });
+    })
+}
+addListeners();
+
+function updateConstants() {
+    A = document.getElementById('a').value;
+    B = document.getElementById('b').value;
+    C = document.getElementById('c').value;
+}
+
+
 function processSurfaceEquations(u, v) {
-    // constant variables for equations
-    const A = 0.3;
-    const B = 0.3;
-    const C = 0.15;
     const x = A * deg2rad(u) * Math.sin(deg2rad(u)) * Math.cos(deg2rad(v));
     const y = B * deg2rad(u) * Math.cos(deg2rad(u)) * Math.cos(deg2rad(v));
     const z = -C * deg2rad(u) * Math.sin(deg2rad(v));
     return { x, y, z };
-
 }
 
 
@@ -26,6 +48,7 @@ function processSurfaceEquations(u, v) {
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
+    this.iNormalBuffer = gl.createBuffer();
     this.count = 0;
 
     this.BufferData = function(vertices) {
@@ -36,16 +59,23 @@ function Model(name) {
         this.count = vertices.length/3;
     }
 
+    this.SetNormalBuffer = function(vertices) {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+    }
+
     this.Draw = function() {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
-   
-        const step = 15;
-        for (let i = 0; i < this.count; i += step) { // draw each line using 15 points
-            gl.drawArrays(gl.LINE_STRIP, i, step + 1); 
-        }
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribNormal);
+
+        gl.drawArrays(gl.TRIANGLES, 0, this.count);
     }
 }
 
@@ -74,7 +104,7 @@ function ShaderProgram(name, program) {
  * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
  */
 function draw() { 
-    gl.clearColor(0,0,0,1);
+    gl.clearColor(0,0,0,0.1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
     /* Set the values of the projection transformation */
@@ -96,33 +126,72 @@ function draw() {
     gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
     
     /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
+    gl.uniform4fv(shProgram.iColor, [1,1,1,1] );
+
+    let A = document.getElementById('a').value
+    let B = document.getElementById('b').value
+    let C = document.getElementById('c').value
+
+    let x = document.getElementById('x').value
+    let y = document.getElementById('y').value
+    let z = document.getElementById('z').value
+    gl.uniform3fv(shProgram.iLightPosition, [x, y, z]);
+
+    gl.uniform3fv(shProgram.iLightDirection, [-1, -1, -1]);
+    let f = document.getElementById('f').value
+    let r = document.getElementById('r').value
+    gl.uniform1f(shProgram.iRange, r);
+    gl.uniform1f(shProgram.iFocus, f);
 
     surface.Draw();
+}
+
+function drawUsingAnimFrame() {
+    draw();
+    window.requestAnimationFrame(drawUsingAnimFrame);
 }
 
 function CreateSurfaceData()
 {
     let vertexList = [];
+    let normalsList = [];
 
     // 0 <= u <= 2PI, -PI <= v <= PI
     const innerStep = 5;
-    for (let i = 0; i <= 360; i+=innerStep) {
-        for (let j = -180; j <= 180; j+=innerStep) {
-            const { x, y, z } = processSurfaceEquations(i, j);
-            vertexList.push(x, y, z);
-        }
-    }
-
-    for (let i = -180; i <= 180; i+=innerStep) {
-        for (let j = 0; j <= 360; j+=innerStep) {
+    
+    for (let i = -180; i <= 180; i += innerStep) {
+        for (let j = 0; j <= 360; j += innerStep) {
             const { x, y, z } = processSurfaceEquations(j, i);
+            const { x: x1, y: y1, z: z1 } = processSurfaceEquations(j + innerStep, i);
+            const { x: x2, y: y2, z: z2 } = processSurfaceEquations(j, i + innerStep);
+            const { x: x3, y: y3, z: z3 } = processSurfaceEquations(j + innerStep, i + innerStep);
+            const n = getNormal(j, i);
+            const n1 = getNormal(j + innerStep, i);
+            const n2 = getNormal(j, i + innerStep);
+            const n3 = getNormal(j + innerStep, i + innerStep);
             vertexList.push(x, y, z);
+            vertexList.push(x1, y1, z1);
+            vertexList.push(x2, y2, z2);
+            vertexList.push(x2, y2, z2);
+            vertexList.push(x1, y1, z1);
+            vertexList.push(x3, y3, z3);
+            normalsList.push(...n, ...n1, ...n2, ...n2, ...n1, ...n3);
         }
     }
 
 
-    return vertexList;
+    return { vertexList, normalsList };
+}
+
+function getNormal(j, i) {
+    const innerStep = 0.0001;
+    const { x, y, z } = processSurfaceEquations(j, i);
+    const { x: x1, y: y1, z: z1 } = processSurfaceEquations(j + innerStep, i);
+    const { x: x2, y: y2, z: z2 } = processSurfaceEquations(j, i + innerStep);
+    const deltaU = [x - x1 / innerStep, y - y1 / innerStep, z - z1 / innerStep];
+    const deltaV = [x - x2 / innerStep, y - y2 / innerStep, z - z2 / innerStep];
+    const normal = m4.normalize(m4.cross(deltaU, deltaV));
+    return normal;
 }
 
 
@@ -134,11 +203,19 @@ function initGL() {
     shProgram.Use();
 
     shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribNormal              = gl.getAttribLocation(prog, "normal");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
     shProgram.iColor                     = gl.getUniformLocation(prog, "color");
 
+    shProgram.iLightPosition             = gl.getUniformLocation(prog, "lightPosition");
+    shProgram.iLightDirection            = gl.getUniformLocation(prog, "lightDirection");
+    shProgram.iRange                     = gl.getUniformLocation(prog, "range");
+    shProgram.iFocus                     = gl.getUniformLocation(prog, "focus");
+
     surface = new Model('Surface');
-    surface.BufferData(CreateSurfaceData());
+    const { vertexList, normalsList } = CreateSurfaceData()
+    surface.BufferData(vertexList);
+    surface.SetNormalBuffer(normalsList);
 
     gl.enable(gl.DEPTH_TEST);
 }
@@ -204,5 +281,5 @@ function init() {
 
     spaceball = new TrackballRotator(canvas, draw, 0);
 
-    draw();
+    drawUsingAnimFrame();
 }
